f00284820bfb242aa14e1b9f0bda3566c808eaaa
Ron Haber <haberrj95@gmail.com>
2020-12-12 20:11:38 +0100

    Changes to work on the javascript pull of csvs

--- Frontend/js/auto_fill.js
+++ Frontend/js/auto_fill.js
@@ -1,43 +1,62 @@
-function handleFiles(files) {
-    // Check for the various File API support.
-    if (window.FileReader) {
-        // FileReader are supported.
-        getAsText(files[0]);
-    } else {
-        alert('FileReader are not supported in this browser.');
-    }
-  }
-
-function getAsText(fileToRead) {
-    var reader = new FileReader();
-    // Read file into memory as UTF-8      
-    reader.readAsText(fileToRead);
-    // Handle errors load
-    reader.onload = loadHandler;
-    reader.onerror = errorHandler;
-}
-
-function loadHandler(event) {
-    var csv = event.target.result;
-    processData(csv);
-}   
-
-function processData(csv) {
-    var allTextLines = csv.split(/\r\n|\n/);
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
     var lines = [];
-    for (var i=0; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(';');
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
             var tarr = [];
-            for (var j=0; j<data.length; j++) {
-                tarr.push(data[j]);
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
             }
             lines.push(tarr);
+        }
     }
-    console.log(lines);
+    // alert(lines);
 }
 
-function errorHandler(evt) {
-    if(evt.target.error.name == "NotReadableError") {
-        alert("Cannot read file !");
-    }
-}
+// function handleFiles(files) {
+//     // Check for the various File API support.
+//     if (window.FileReader) {
+//         // FileReader are supported.
+//         getAsText(files[0]);
+//     } else {
+//         alert('FileReader are not supported in this browser.');
+//     }
+//   }
+
+// function getAsText(fileToRead) {
+//     var reader = new FileReader();
+//     // Read file into memory as UTF-8      
+//     reader.readAsText(fileToRead);
+//     // Handle errors load
+//     reader.onload = loadHandler;
+//     reader.onerror = errorHandler;
+// }
+
+// function loadHandler(event) {
+//     var csv = event.target.result;
+//     processData(csv);
+// }   
+
+// function processData(csv) {
+//     var allTextLines = csv.split(/\r\n|\n/);
+//     var lines = [];
+//     for (var i=0; i<allTextLines.length; i++) {
+//         var data = allTextLines[i].split(';');
+//             var tarr = [];
+//             for (var j=0; j<data.length; j++) {
+//                 tarr.push(data[j]);
+//             }
+//             lines.push(tarr);
+//     }
+//     console.log(lines);
+// }
+
+// function errorHandler(evt) {
+//     if(evt.target.error.name == "NotReadableError") {
+//         alert("Cannot read file !");
+//     }
+// }

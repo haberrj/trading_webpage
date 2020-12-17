87f4480b071b05fec4412d9208b3f60a971be6e2
Ron Haber <haberrj95@gmail.com>
2020-12-17 19:49:27 +0100

    Fixed the docker and webpage requirements

--- Backend/python/run_sqlbd.py
+++ Backend/python/run_sqlbd.py
@@ -7,13 +7,15 @@
 import os, sys
 import time
 
+os.system("/etc/init.d/apache2 restart")
+
 while(True):
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/CSV_Transactions/BTC_transactions.csv -n BTC_Transactions")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/CSV_Transactions/ETH_transactions.csv -n ETH_Transactions")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/CSV_Transactions/LTC_transactions.csv -n LTC_Transactions")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/CSV_Transactions/BNB_transactions.csv -n BNB_Transactions")
     # Prices
-    os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/BTC_Realtime.csv -n BTC_Realtime)
+    os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/BTC_Realtime.csv -n BTC_Realtime")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/ETH_Realtime.csv -n ETH_Realtime")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/LTC_Realtime.csv -n LTC_Realtime")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/BNB_Realtime.csv -n BNB_Realtime")
@@ -22,6 +24,5 @@ while(True):
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/Balances/ETH_Balance.csv -n ETH_Balance")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/Balances/LTC_Balance.csv -n LTC_Balance")
     os.system("python3  /scripts/Backend/python/create_sqldb.py -c /database_files/Binance/Actual/Balances/BNB_Balance.csv -n BNB_Balance")
-
     print("Complete!")
-    time.sleep(30) # sleep for 30s before updating
+    time.sleep(30) # sleep for 30 seconds before executing
--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM php:7.2-apache
 LABEL maintainer="Ron Haber"
 
 RUN apt-get update
-RUN apt-get install -y git python3 nano
+RUN apt-get install -y cron python3 nano
 
 RUN mkdir /database_files/
 RUN mkdir /database_files/Binance
@@ -11,10 +11,12 @@ RUN mkdir /scripts
 
 ADD Backend /scripts/Backend
 
-RUN chmod +x /scripts/Backend/python/create_relevant_database.sh
+RUN chmod +x /scripts/Backend/python/run_sqlbd.py
+# RUN chmod +x /scripts/Backend/python/create_sqlbd.py
 
 COPY index.php /var/www/html/
 COPY Frontend /var/www/html/Frontend
 WORKDIR /scripts/Backend/python/
 
-# ENTRYPOINT [ "create_relevant_database.sh" ]
+ENTRYPOINT ["python3", "./run_sqlbd.py"]
+CMD ["/etc/init.d/apache2", "restart"]

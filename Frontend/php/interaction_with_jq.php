d30fa2d457d67d1e8f28fa1bd4af8637d2c9e8aa
Ron Haber <haberrj95@gmail.com>
2020-12-18 14:50:41 +0100

    Cleaned up code and added javascript

---
+++ Frontend/php/interaction_with_jq.php
@@ -0,0 +1,49 @@
+<?php
+    include "output_calls.php";
+    include "read_csv.php";
+
+    if(!empty($_REQUEST['curr']) && !empty($_REQUEST['type'])){
+        $curr = $_REQUEST['curr'];
+        $type = $_REQUEST['type'];
+        switch($curr){
+            case 'BTC':
+                $file1 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/BTC_Transactions.db";
+                $file2 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/BTC_Realtime.db";
+                $file3 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/BTC_Balance.db";
+                break;
+            case 'ETH':
+                $file1 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/ETH_Transactions.db";
+                $file2 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/ETH_Realtime.db";
+                $file3 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/ETH_Balance.db";
+                break;
+            case 'LTC':
+                $file1 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/LTC_Transactions.db";
+                $file2 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/LTC_Realtime.db";
+                $file3 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/LTC_Balance.db";
+                break;
+            case 'BNB':
+                $file1 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/BNB_Transactions.db";
+                $file2 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/BNB_Realtime.db";
+                $file3 = "/home/ronhaber/Documents/Crypto_Docs/Binance/Databases/BNB_Balance.db";                
+                break;
+        }
+        switch($type){
+            case 'cash':
+                Print_Cash($file3);
+                break;
+            case 'balance':
+                Print_Balance($file1);
+                break;
+            case 'current_price':
+                Print_Current_Price($file2);
+                break;
+            case 'holding_price':
+                Print_Holding_Price($file1);
+                break;
+            case 'networth':
+                Print_Networth($file1, $file2, $file3);
+                break;
+        }
+    }
+    
+?>
--- Frontend/php/output_calls.php
+++ Frontend/php/output_calls.php
@@ -1,27 +1,49 @@
 <?php
     include 'read_csv.php';
 
-    function BTC_Cash(){
-        $file = "/database_files/BTC_Balance.db";
+    function Print_Cash($file){
         $connection = connectToDB($file);
         $values = getLastCashValues($connection);
         echo getCash($values);
         closeConnection($connection);
     }
     
-    function BTC_Balance(){
-        $file = "/database_files/BTC_Transactions.db";
+    function Print_Balance($file){
         $connection = connectToDB($file);
         $values = getLastTransactionValues($connection);
         echo getBalance($values);
         closeConnection($connection);
     }
 
-    function BTC_Current_Price(){
-        $file = "/database_files/BTC_Realtime.db";
+    function Print_Current_Price($file){
         $connection = connectToDB($file);
         $values = getLastPriceValues($connection);
         echo getPrice($values);
         closeConnection($connection);
     }
+
+    function Print_Holding_Price($file){
+        $connection = connectToDB($file);
+        $values = getLastTransactionValues($connection);
+        $last_holding_price = getHoldingPrice($values);
+        echo $last_holding_price;
+        if($last_holding_price != "N/A"){
+            echo " €";
+        }
+        closeConnection($connection);
+    }
+
+    function Print_Networth($file, $file2, $file3){
+        $connection = connectToDB($file);
+        $trans_values = getLastTransactionValues($connection);
+        closeConnection($connection);
+        $connection = connectToDB($file2);
+        $price_values = getLastPriceValues($connection);
+        closeConnection($connection);
+        $connection = connectToDB($file3);
+        $cash_values = getLastCashValues($connection);
+        closeConnection($connection);
+        echo calculateNetworth(getBalance($trans_values), getPrice($price_values), getCash($cash_values));
+    }
+    
 ?>
